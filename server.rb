119923425f2b91ca901b93171e1f0788729e63af
Mohd Sholihin Shah <kidsboy95@yahoo.com>
2016-06-14 23:48:14 +0800

    Add files via upload

---
+++ server.rb
@@ -0,0 +1,10 @@
+module WEBSocket
+  class Server < Base::Server
+    extend Forwardable
+    def_delegators :@server,  :listen,  :sysaccept, :close,  :closed?,  :addr
+
+    def initialize hostname, port
+      @server = TCPServer.new hostname, port
+    end
+  end
+end
